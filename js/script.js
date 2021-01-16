d0f52d4491aac853f363353e112e89576b8a5ff0
Mohammad Al-Yamani <mohammad.alyamani95@gmail.com>
2021-01-16 14:43:12 +0200

    Royal & Gallary Section End

--- js/script.js
+++ js/script.js
@@ -3,4 +3,23 @@ $(document).ready(function() {
         $(this).addClass('active-link');
         $(this).parent().siblings().children().removeClass('active-link');
     });
+
+    var nav_offset_top = $('.navbar ').height() + 50;
+    console.log(nav_offset_top);
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= nav_offset_top) {
+            $(".custom-nav").removeClass("position-absolute container");
+
+            $(".custom-nav").addClass("position-fixed container-fulied");
+            $(".navbar").addClass("container");
+
+        } else {
+            $(".custom-nav").addClass("position-absolute container");
+            $(".custom-nav").removeClass("position-fixed container-fulied");
+            $(".navbar").removeClass("container");
+        }
+    });
 });
