52bf00dc0957b18a8887e07d3e904c3301fa4052
Mohammad Al-Yamani <mohammad.alyamani95@gmail.com>
2021-01-17 15:33:29 +0200

    Add wow Animation & End Project

--- js/script.js
+++ js/script.js
@@ -18,12 +18,36 @@ $(document).ready(function() {
             $(".custom-nav").removeClass("position-absolute container");
 
             $(".custom-nav").addClass("position-fixed container-fulied");
+            $(".navbar-nav").css('visibility', 'visible');
+            $(".navbar-brand").css('visibility', 'visible');
             $(".navbar").addClass("container");
 
         } else {
             $(".custom-nav").addClass("position-absolute container");
             $(".custom-nav").removeClass("position-fixed container-fulied");
             $(".navbar").removeClass("container");
+        }
+    });
+
+    $('footer #send').click(function() {
+        var email = $('footer #email').val();
+
+        if (email == '') {
+            $('.alert-dismissible').remove();
+            $('.newsletter').append(`
+                <div class="alert mt-4 alert-danger alert-dismissible fade show" role="alert">
+                    Enter Your <strong>Email!</strong> Please.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `);
+        } else {
+            $('.alert-dismissible').remove();
+            $('.newsletter').append(`
+                <div class="alert mt-4 alert-success alert-dismissible fade show" role="alert">
+                    Sent <strong>Email!</strong> Succesfully.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `);
         }
     });
 });
